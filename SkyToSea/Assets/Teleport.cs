f49952a9d94ce351396f4b9ae07bf0266467ce88
Jon Friedman <jonfgit@gmail.com>
2017-03-31 20:17:22 -0700

    Built landscape, water, and teleports

---
+++ SkyToSea/Assets/Teleport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+    public Transform TeleportTgt;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            print(other.tag + " entered the teleporter");
+            other.transform.position = TeleportTgt.position;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
